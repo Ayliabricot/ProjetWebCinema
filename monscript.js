fef2c7406e579f0e17a830857bf6723bc90b3a2e
Ayliabricot <you@example.com>
2026-05-18 03:08:16 +0200

    Ajout interface recherche + tentative connexion api (pas encore utilisée, je ne sais pas si elle fonctionne)

--- monscript.js
+++ monscript.js
@@ -1,1 +1,13 @@
+async function loadData(){
+    let ok="Ok";
+    let main=document.querySelector("main");
 
+    const API_KEY = "4ec6eac902806dbb1cbe874e60ac5cf2";
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr-FR&page=1`)
+        .then(response => response.json())
+        .catch(error => ok="Erreur : " + error);
+        
+    console.log("Les données ont bien été récupérées");
+}
+
+loadData();
