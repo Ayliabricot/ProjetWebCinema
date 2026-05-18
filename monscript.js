a40d7be84a313ad7c988510ffbda8aa26c35373e
Ayliabricot <you@example.com>
2026-05-18 23:47:44 +0200

    Après de très longues péripéties la section des séries TV est là. J'ai eu pas mal de difficultés car j'ai dû revoir le fonctionnement de pas mal de petites choses dans mon code. J'avoue avoir sollicité l'ia pour lui demander les différences dans l'url selon les séries / tendances car je me suis perdue dans la doc : j'ai cependant tout fait moi-même sur le reste (en espérant que cela vous semble cohérent).

--- monscript.js
+++ monscript.js
@@ -1,30 +1,58 @@
-async function loadData(tendancesChoix){
+async function loadData(choix,choixInverse){
     let boutonActif;
     let boutonInactif;
     let tendanceValeurUrl;
 
-    let boutonAujourdhui=document.querySelector("#aujourdhui");
-    let boutonSemaine=document.querySelector("#semaine");
-
-    boutonAujourdhui.addEventListener ('click',()=>{
-            console.log("aujourdhui");
-            loadData("aujourdhui");
-    });
-    boutonSemaine.addEventListener ('click',()=>{
-            console.log("semaine");
-            loadData("semaine");
-    });
-
-    if (tendancesChoix=="aujourdhui"){
-        boutonActif=boutonAujourdhui;
-        boutonInactif=boutonSemaine;
-        tendanceValeurUrl="day";
+    switch (choix){
+        case "aujourdhui":
+            boutonActif=document.querySelector("#aujourdhui");
+            boutonInactif=document.querySelector("#semaine");
+            tendanceValeurUrl="trending/movie/day";
+            break;
+        case "semaine":
+            boutonActif=document.querySelector("#semaine");
+            boutonInactif=document.querySelector("#aujourdhui");
+            tendanceValeurUrl="trending/movie/week";
+            break;
+        case "meilleures":
+            boutonActif=document.querySelector("#meilleures");
+            boutonInactif=document.querySelector("#populaires");
+            tendanceValeurUrl="tv/top_rated";
+            break;
+        case "populaires":
+            boutonActif=document.querySelector("#populaires");
+            boutonInactif=document.querySelector("#meilleures");
+            tendanceValeurUrl="tv/popular";
+            break;
     }
-    else if (tendancesChoix=="semaine"){
-        boutonActif=boutonSemaine;
-        boutonInactif=boutonAujourdhui;
-        tendanceValeurUrl="week";
-    }
+
+    boutonActif.addEventListener ('click',()=>{
+            console.log(choix);
+            loadData(choix,choixInverse);
+    });
+    boutonInactif.addEventListener ('click',()=>{
+            console.log(choixInverse);
+            loadData(choixInverse,choix);
+    });
+
+    changerBoutons(boutonActif,boutonInactif);
+
+    let ok="Ok";
+    let main=document.querySelector("main");
+
+    const API_KEY = "4ec6eac902806dbb1cbe874e60ac5cf2";
+    const data = await fetch(`https://api.themoviedb.org/3/${tendanceValeurUrl}?api_key=${API_KEY}&language=fr-FR`)
+        .then(response => response.json())
+        .catch(error => ok="Erreur : " + error);
+        
+    console.log("Les données ont bien été récupérées");
+   
+    afficher(data.results,choix,choixInverse);
+}
+
+function changerBoutons(boutonActif, boutonInactif){
+    console.log("ici");
+    console.log(boutonActif.textContent);
     boutonActif.style.backgroundColor = "#032541";
     boutonActif.style.borderColor = "white";
     boutonActif.style.color = "white";
@@ -32,29 +60,35 @@ async function loadData(tendancesChoix){
     boutonInactif.style.backgroundColor = "white";
     boutonInactif.style.borderColor = "#032541";
     boutonInactif.style.color = "#032541";
-
-    let ok="Ok";
-    let main=document.querySelector("main");
-
-    const API_KEY = "4ec6eac902806dbb1cbe874e60ac5cf2";
-    const data = await fetch(`https://api.themoviedb.org/3/trending/movie/${tendanceValeurUrl}?api_key=${API_KEY}&language=fr-FR`)
-        .then(response => response.json())
-        .catch(error => ok="Erreur : " + error);
-        
-    console.log("Les données ont bien été récupérées");
-   
-    afficherFilms(data.results);
 }
 
-function afficherFilms(films) {
-    const section = document.querySelector("#listeFilms");
+function afficher(films,choix,choixInverse) {
+    let choixListe;
+    let titre;
+    let date;
+    
+    if (choix=="aujourdhui" || choixInverse=="aujourdhui"){
+        choixListe="#listeFilms";
+    }
+    else if (choix=="meilleures" || choixInverse=="meilleures"){
+        choixListe="#listeSeries";
+    }
+    const section = document.querySelector(choixListe);
     section.innerHTML='';
     const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
     
     const conteneur = document.createElement("div");
-    conteneur.id = "listeFilms";
+    conteneur.id = choixListe.slice(1);
  
     films.slice(0,4).forEach(film => {
+        if (choix=="aujourdhui" || choixInverse=="aujourdhui"){
+            titre=film.title;
+            date=film.release_date;
+        }
+        else if (choix=="meilleures" || choixInverse=="meilleures"){
+            titre=film.name;
+            date=film.first_air_date;
+        }
         const carte = document.createElement("div");
         carte.classList.add("carteFilm");
  
@@ -68,8 +102,8 @@ function afficherFilms(films) {
                 </div>
             </div>
             <div class="infoFilm">
-                <h3>${film.title}</h3>
-                <p>${new Date(film.release_date).toLocaleDateString('fr-FR', { day: 'numeric', month: 'short', year: 'numeric' })}</p>
+                <h3>${titre}</h3>
+                <p>${new Date(date).toLocaleDateString('fr-FR', { day: 'numeric', month: 'short', year: 'numeric' })}</p>
             </div>
         `;
  
@@ -80,4 +114,5 @@ function afficherFilms(films) {
 }
  
 
-loadData("semaine");
+loadData("semaine","aujourdhui");
+loadData("populaires","meilleures")
